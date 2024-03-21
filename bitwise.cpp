9bb5b15c4feeb6f7feed35d214e4506ff7724733
ani3321r <ani3321r@gmail.com>
2024-03-21 22:57:21 +0530

    bitwise operators

---
+++ bitwise.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    unsigned int x = 8;
+    unsigned int y = 3;// we mainly use unsigned int for bitwise operations
+
+    unsigned int z;
+    /*
+    z = x & y /bitwise and
+    z = x | y /bitwise or
+    z = x ^ y /bitwise XOR
+    z = ~x /not op
+    z = y << 1 /left shift of 1
+    z = y >> 1 /right shift of 1
+    */
+}
